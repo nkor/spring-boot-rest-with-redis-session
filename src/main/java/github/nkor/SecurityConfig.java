4161ad6a7fdd875f97ed423ed41a586306edb450
nkor <nikolay.kormshchikov@gmail.com>
2019-04-21 16:39:47 +0600

    custom CustomUserDetailsService and real SecurityConfig

--- src/main/java/github/nkor/SecurityConfig.java
+++ src/main/java/github/nkor/SecurityConfig.java
@@ -1,5 +1,6 @@
 package github.nkor;
 
+import github.nkor.services.CustomUserDetailsService;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
@@ -7,36 +8,49 @@ import org.springframework.security.config.annotation.method.configuration.Enabl
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.session.web.http.HeaderHttpSessionIdResolver;
 import org.springframework.session.web.http.HttpSessionIdResolver;
 
 @Configuration
 @EnableWebSecurity
-@EnableGlobalMethodSecurity(prePostEnabled = true)
-public class TestSecurityConfig extends WebSecurityConfigurerAdapter {
+@EnableGlobalMethodSecurity(prePostEnabled = true) // enable @PreAuthorize and @PostAuthorize annotations
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    private final CustomUserDetailsService customUserDetailsService;
+
+    public SecurityConfig(CustomUserDetailsService customUserDetailsService) {
+        this.customUserDetailsService = customUserDetailsService;
+    }
 
     @Override
     protected void configure(AuthenticationManagerBuilder auth) throws Exception {
-        auth.inMemoryAuthentication()
-                .withUser("nkor")
-                .password("123qwe")
-                .authorities("ADMIN1");
+        // apply custom user details service
+        auth.userDetailsService(customUserDetailsService);
     }
 
     @Override
     protected void configure(HttpSecurity http) throws Exception {
         http
+                // enable HTTP Basic Authentication
                 .httpBasic()
-                .and()
+                    .and()
+
+                // any requests requires user authentication except '/user/register'
                 .authorizeRequests()
-                .anyRequest()
-                .authenticated()
-                .and()
+                    .antMatchers("/user/register")
+                    .permitAll()
+                    .anyRequest()
+                    .authenticated()
+                    .and()
+
+                // disable useless csrf protection for REST application
                 .csrf()
-                .disable();
+                    .disable();
     }
 
+    // token-based session by using 'X-Auth-Token' header
     @Bean
     public HttpSessionIdResolver httpSessionIdResolver() {
         return HeaderHttpSessionIdResolver.xAuthToken();
@@ -44,16 +58,6 @@ public class TestSecurityConfig extends WebSecurityConfigurerAdapter {
 
     @Bean
     public PasswordEncoder passwordEncoder() {
-        return new PasswordEncoder() {
-            @Override
-            public String encode(CharSequence rawPassword) {
-                return rawPassword.toString();
-            }
-
-            @Override
-            public boolean matches(CharSequence rawPassword, String encodedPassword) {
-                return rawPassword.equals(encodedPassword);
-            }
-        };
+        return new BCryptPasswordEncoder();
     }
 }
---
+++ src/main/java/github/nkor/services/CustomUserDetailsService.java
@@ -0,0 +1,96 @@
+package github.nkor.services;
+
+import github.nkor.models.Permission;
+import github.nkor.models.Role;
+import github.nkor.models.User;
+import github.nkor.repository.UserRepository;
+import org.springframework.security.core.GrantedAuthority;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.io.Serializable;
+import java.util.Collection;
+import java.util.Collections;
+import java.util.Set;
+import java.util.stream.Collectors;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+
+    private final UserRepository userRepository;
+
+    public CustomUserDetailsService(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException {
+        User userWithPermissions = userRepository.findUserWithPermissions(email);
+
+        if (userWithPermissions == null) {
+            throw new UsernameNotFoundException("User with email '" + email + "' not foud");
+        }
+
+        return new UserDetailsWithUserEntity(userWithPermissions);
+    }
+
+    private static final class UserDetailsWithUserEntity implements UserDetails, Serializable {
+        private User user;
+
+        public UserDetailsWithUserEntity(User user) {
+            this.user = user;
+        }
+
+        @Override
+        public Collection<? extends GrantedAuthority> getAuthorities() {
+            Set<Role> roles = user.getRoles();
+
+            if (roles == null) {
+                return Collections.emptySet();
+            }
+            // flat permissions
+            Set<Permission> permissions = roles.stream()
+                    .filter(r -> r.getPermissions() != null)
+                    .flatMap(r -> r.getPermissions().stream())
+                    .collect(Collectors.toSet());
+
+            // map permissions to SimpleGrantedAuthority
+            return permissions.stream()
+                    .map(p -> new SimpleGrantedAuthority(p.getName()))
+                    .collect(Collectors.toSet());
+        }
+
+        @Override
+        public String getPassword() {
+            return user.getPassword();
+        }
+
+        @Override
+        public String getUsername() {
+            return user.getEmail();
+        }
+
+        @Override
+        public boolean isAccountNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isAccountNonLocked() {
+            return !user.isLocked();
+        }
+
+        @Override
+        public boolean isCredentialsNonExpired() {
+            return true;
+        }
+
+        @Override
+        public boolean isEnabled() {
+            return true;
+        }
+    } // end UserDetailsWithUserEntity class
+}
