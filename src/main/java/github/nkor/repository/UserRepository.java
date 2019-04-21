2ee1ebc06a653a95d10dd1a8e185f2384cacaf1e
nkor <nikolay.kormshchikov@gmail.com>
2019-04-21 16:37:56 +0600

    UserRepository with findUserWithPermissions

---
+++ src/main/java/github/nkor/repository/UserRepository.java
@@ -0,0 +1,15 @@
+package github.nkor.repository;
+
+import github.nkor.models.User;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface UserRepository extends CrudRepository<User, Integer> {
+
+    @Query("select u from User u" +
+            " left join fetch u.roles r" +
+            " left join fetch r.permissions" +
+            " where u.email = :email")
+    User findUserWithPermissions(@Param("email") String email);
+}
